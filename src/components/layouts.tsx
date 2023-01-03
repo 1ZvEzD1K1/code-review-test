e1bd9ff53eabe66196ff41dfe5cc1ecb97b15dc9
D. Voronov <devdivoron@gmail.com>
2023-01-03 16:17:19 +0200

    change 6 - optimize 2 (use memo)

--- src/components/layouts.tsx
+++ src/components/layouts.tsx
@@ -11,7 +11,7 @@ export const CenteredLayout = ({
   // TODO is there a better way to fill available remaining height?
   // scroll height seems bugged :\
   // -------------------------------------------------------------
-  // DONE : h-screen + set style #root in index.css
+  // DONE : h-screen
   <div
     className={clsx(
       'flex flex-col items-center justify-center h-screen pb-32 text-slate-700',
--- src/pages/optimize-2/optimize-2.tsx
+++ src/pages/optimize-2/optimize-2.tsx
@@ -1,14 +1,16 @@
-import { useEffect, useState } from 'react';
+import { useEffect, useState, memo } from 'react';
 import { CenteredLayout } from '~/components';
 
 // TODO how can we optimize, prevent re-rendering ExpensiveComponent
 // by changing component structure ?
+// -----------------------------------
+// DONE: use React.memo()
 
-const ExpensiveComponent = () => {
+const ExpensiveComponent = memo(() => {
   const now = performance.now();
   while (performance.now() - now < 100) {}
   return <div>Ohh.. so expensive</div>;
-};
+});
 
 export const Optimize2 = () => {
   const [scrollTop, setScrollTop] = useState(0);
